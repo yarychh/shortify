e55747795f10ad5b3d03402f98b3ec431bfe85e2
Mike Yarovenko <yarovenkomikhail@gmail.com>
2024-05-12 17:17:25 +0200

    added cloudflare adapter

--- src/app.d.ts
+++ src/app.d.ts
@@ -7,6 +7,13 @@ declare global {
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
+		interface Platform {
+			env: {};
+			context: {
+				waitUntil(promise: Promise<any>): void;
+			};
+			caches: CacheStorage & { default: Cache };
+		}
 	}
 }
 
